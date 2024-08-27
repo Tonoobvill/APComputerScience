62f4c9ad9f4f120449f566ae40352a8f5aca7125
Tonoobvill <86608694+Tonoobvill@users.noreply.github.com>
2024-08-27 16:27:59 -0400

    Create StudentInfoCard.java

---
+++ StudentInfoCard.java
@@ -0,0 +1,50 @@
+/**
+ * Name: Timo Fauxnam
+ * Date: August 26, 2024
+ * Class: AP Computer Science A
+ * Assignment: 01.04 Student Contact Card / StudentInfoCard
+ * Purpose: Give my info
+ */
+
+ public class StudentInfoCard {
+    public static void main(String[] args) {
+        // Banner
+        System.out.println("*********************************************");
+        System.out.println("*                                           *");
+        System.out.println("*            Student Information            *");
+        System.out.println("*                                           *");
+        System.out.println("*********************************************\n");
+        
+        // Personal Information
+        System.out.print("Name: Timo Fauxnam   ");
+        System.out.print("Birthday: October 17, 2003   ");
+        System.out.print("Age: 21\n\n");
+
+        // School Information
+        System.out.print("School: Kirkland Ranch Academy of Innovation   ");
+        System.out.print("Grade: 12th   ");
+        System.out.print("City: Wesley Chapel\n\n");
+
+        // Contact Information
+        System.out.println("Cell Phone: 555-555-5555, Available: 3:30pm - 10pm Tue, Wed, Fri");
+        System.out.print("Email: tonoobvill@gmail.com\n\n");
+
+        // Parent's Contact Information
+        System.out.println("Mom's contact information");
+        System.out.println("Cell Phone: 555-555-5555, texting better than calling");
+        System.out.print("Email: [nuh uh]@gmail.com\n\n");
+
+        // Parent's Contact Information
+        System.out.println("Dad's contact information");
+        System.out.println("Cell Phone: 555-555-5555, texting better than calling");
+        System.out.print("Email: [nope]@gmail.com\n\n");
+
+        // Academic Information
+        System.out.println("Highest Level Math Class: ALG 2 HON");
+        System.out.print("Programming Experience: Comp Sci Princ., learning HTML, CSS, JS, and Python on own time.\n\n");
+
+        // Why Taking the Course
+        System.out.println("I am taking APCS because I am interested in learning more about programming.");
+        System.out.print("I think I want to major in Computer Science or Computer Engineering.");
+    }
+}
